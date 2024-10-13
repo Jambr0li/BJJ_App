690d1002018960c39f903c94f934d1571c999057
Jambr0li <jasonkovalenko@gmail.com>
2024-10-13 13:20:46 -0700

    basic functionality

--- data.py
+++ data.py
@@ -1,13 +1,16 @@
 items = [
     {
+        'id': '1',
         'title': 'Tying the Belt',
         'video_url': 'https://www.youtube.com/watch?v=COqdpkfDevo&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=1'
     },
     {
+        'id': '2',
         'title': 'Basic Jiu Jitsu Postures',
         'video_url': 'https://www.youtube.com/watch?v=3oQJsXMzWgo&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=2&pp=iAQB'
     },
     {
+        'id': '3',
         'title': 'Basic Movements (Teeter Totter)',
         'video_url': 'https://www.youtube.com/watch?v=-cdAd4w--VE&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=3&t=2s&pp=iAQB'
     }
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import os
 from config import Config
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for
+from flask import Flask, redirect, render_template, session, url_for, request
 from flask_flatpages import FlatPages
 from data import items
 
@@ -58,9 +58,40 @@ def logout():
         )
     )
 
-@app.route("/")
+@app.before_request
+def before_request():
+    if 'progress' not in session:
+        session['progress'] = {item['id']: {'learned': False, 'proficient': False} for item in items}
+
+@app.route('/', methods =['POST','GET'])
 def home():
-    return render_template("classes.html", items=items, session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    if request.method == 'POST':
+        for item in items:
+            item_id = item['id']
+            learned = request.form.get(f'learned_{item_id}') == 'on'
+            proficient = request.form.get(f'proficient_{item_id}') == 'on'
+            session['progress'][item_id]['learned'] = learned
+            session['progress'][item_id]['proficient'] = proficient
+        
+        session.modified = True
+        return redirect(url_for('home'))
+    total_items = len(items)
+    learned_count = sum(1 for progress in session['progress'].values() if progress['learned'])
+    proficient_count = sum(1 for progress in session['progress'].values() if progress['proficient'])
+    learned_progress = (learned_count / total_items) * 100 if total_items > 0 else 0
+    proficient_progress = (proficient_count / total_items) * 100 if total_items > 0 else 0
+
+    return render_template('classes.html',
+                           items=items,
+                           progress=session['progress'],
+                           learned_progress=learned_progress,
+                           proficient_progress=proficient_progress,
+                           total_items=total_items)
+
+
+# @app.route("/")
+# def home():
+#     return render_template("classes.html", items=items, session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=env.get("PORT", 3000), debug=True)
