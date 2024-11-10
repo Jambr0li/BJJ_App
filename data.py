e689f90f615a7a42856fc484b120199c41778d60
Jambr0li <jasonkovalenko@gmail.com>
2024-11-10 14:36:49 -0800

    It's looking pretty good! I need to add the 80ish videos to my data.py tho and the hookup a database

--- data.py
+++ data.py
@@ -18,5 +18,61 @@ items = [
         'id': '4',
         'title': 'Basic Movements (UPA)',
         'video_url': 'https://www.youtube.com/watch?v=c058e5pbORY&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=4&pp=iAQB'
+    },
+    {
+        'id': '5',
+        'title': 'Basic Movements (Rolls - Forward and Backward)',
+        'video_url': 'https://www.youtube.com/watch?v=EOd9ADCTvFg&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=5&pp=iAQB'
+    },
+    {
+        'id': '6',
+        'title': 'Basic Movements (4 Points Base)',
+        'video_url': 'https://www.youtube.com/watch?v=-kwQb6gIbbU&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=6&pp=iAQB'
+    },
+    {
+        'id': '7',
+        'title': 'Basic Movements - Mount Armlock Drills',
+        'video_url': 'https://www.youtube.com/watch?v=y4rumTE_lh4&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=7&pp=iAQB'
+    },
+    {
+        'id': '8',
+        'title': 'Basic Movements (Stand and Base "Technical Standup")',
+        'video_url': 'https://www.youtube.com/watch?v=kJ68PZjeNZU&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=8&pp=iAQB'
+    },
+    {
+        'id': '9',
+        'title': 'Basic Movements (Elbow Escape "Shrimping")',
+        'video_url': 'https://www.youtube.com/watch?v=SYRulxArSUw&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=9&pp=iAQB'
+    },
+    {
+        'id': '10',
+        'title': 'Basic Cross Choke Details',
+        'video_url': 'https://www.youtube.com/watch?v=dF477LHpKjM&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=10&pp=iAQB'
+    },
+    {
+        'id': '11',
+        'title': 'Standing Defense (Overhead Club - Close the Distance)',
+        'video_url': 'https://www.youtube.com/watch?v=0IqAbeVFRUk&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=11&pp=iAQB'
+    },
+    {
+        'id': '12',
+        'title': 'Standing Defense (Kick Defense - Front)',
+        'video_url': 'https://www.youtube.com/watch?v=ZS8S-vezf8o&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=12&pp=iAQB'
+    },
+    {
+        'id': '13',
+        'title': 'Standing Defense (Kick Defense - Low)',
+        'video_url': 'https://www.youtube.com/watch?v=0_679NkT0ZU&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=13&pp=iAQB'
+    },
+    {
+        'id': '14',
+        'title': 'Standing Defense (Kick Defense - High)',
+        'video_url': 'https://www.youtube.com/watch?v=xMA00_sJUdE&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=14&pp=iAQB'
+    },
+    {
+        'id': '15',
+        'title': 'Standing Defense (Leg Check - Close the Gap)',
+        'video_url': 'https://www.youtube.com/watch?v=q2IhKPEMdOM&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=15&pp=iAQB'
     }
+
 ]
