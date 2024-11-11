d22b1b7fba246a653c975efc2cd24423a7a7f767
Jambr0li <jasonkovalenko@gmail.com>
2024-11-10 22:50:38 -0800

    completed data set next need to integrate database to store progress

--- data.py
+++ data.py
@@ -73,6 +73,380 @@ items = [
         'id': '15',
         'title': 'Standing Defense (Leg Check - Close the Gap)',
         'video_url': 'https://www.youtube.com/watch?v=q2IhKPEMdOM&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=15&pp=iAQB'
-    }
-
+    },
+    {
+        'id': '16',
+        'title': 'Standing Defense (Punch Defense - Straight)',
+        'video_url': 'https://www.youtube.com/watch?v=RfZi8yEEqI4&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=16&pp=iAQB'
+    },
+    {
+        'id': '17',
+        'title': 'Standing Defense (Punch Defense - Roundhouse)',
+        'video_url': 'https://www.youtube.com/watch?v=vHeE-NAa4Ks&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=17&pp=iAQB'
+    },
+    {
+        'id': '18',
+        'title': 'Standing Defense (Hair or Ear Grab)',
+        'video_url': 'https://www.youtube.com/watch?v=MitlSX82PrQ&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=18&t=1s&pp=iAQB'
+    },
+    {
+        'id': '19',
+        'title': 'Standing Defense (Double Leg Takedown - Bajana)',
+        'video_url': 'https://www.youtube.com/watch?v=BKHF4yOAVCQ&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=19&pp=iAQB'
+    },
+    {
+        'id': '20',
+        'title': 'Standing Defense (Headlock - Lift Opponent)',
+        'video_url': 'https://www.youtube.com/watch?v=UndOnVIudpo&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=20&pp=iAQB'
+    },
+    {
+        'id': '21',
+        'title': 'Standing Defense (Headlock Punch)',
+        'video_url': 'https://www.youtube.com/watch?v=QEua3UgO2Ig&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=21&pp=iAQB'
+    },
+    {
+        'id': '22',
+        'title': 'Standing Defense (T Position - Hip Throw)',
+        'video_url': 'https://www.youtube.com/watch?v=_RzVYTNDX5g&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=22&pp=iAQB'
+    },
+    {
+        'id': '23',
+        'title': 'Standing Defense (T Position - Leg Throw)',
+        'video_url': 'https://www.youtube.com/watch?v=S_E_zk8HkRU&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=23&pp=iAQB'
+    },
+    {
+        'id': '24',
+        'title': 'Standing Defense (T Position - Sit Down)',
+        'video_url': 'https://www.youtube.com/watch?v=G7faHiIYA5Y&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=24&pp=iAQB'
+    },
+    {
+        'id': '25',
+        'title': 'Standing Defense (Front Bear Hug - Arms Free)',
+        'video_url': 'https://www.youtube.com/watch?v=78OtmRLP1Vc&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=25&pp=iAQB'
+    },
+    {
+        'id': '26',
+        'title': 'Standing Defense (Front Bear Hug - Arms Pinned)',
+        'video_url': 'https://www.youtube.com/watch?v=luQ4t4KX-HQ&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=26&pp=iAQB'
+    },
+    {
+        'id': '27',
+        'title': 'Standing Defense (Rear Bear Hug - Arms Pinned)',
+        'video_url': 'https://www.youtube.com/watch?v=0XgSnHKNemw&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=27&pp=iAQB'
+    },
+    {
+        'id': '28',
+        'title': 'Standing Defense (Rear Bear Hug - Opponent Lifts You Up)',
+        'video_url': 'https://www.youtube.com/watch?v=q-jsSJlr-v0&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=28&pp=iAQB'
+    },
+    {
+        'id': '29',
+        'title': 'Standing Defense (Rear Naked Choke - Flip Opponent)',
+        'video_url': 'https://www.youtube.com/watch?v=kIwgWD2dYOI&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=29&pp=iAQB'
+    },
+    {
+        'id': '30',
+        'title': 'Standing Defense (Rear Naked Choke - Bending Backwards)',
+        'video_url': 'https://www.youtube.com/watch?v=QeemWm0H0Kg&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=30&pp=iAQB'
+    },
+    {
+        'id': '31',
+        'title': 'Standing Defense (Neck - Pinned to Wall with One Hand)',
+        'video_url': 'https://www.youtube.com/watch?v=F7wAoboKuQc&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=31&pp=iAQB'
+    },
+    {
+        'id': '32',
+        'title': 'Standing Defense (Two Handed Choke)',
+        'video_url': 'https://www.youtube.com/watch?v=hfQR2MFkMDw&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=32&pp=iAQB'
+    },
+    {
+        'id': '33',
+        'title': 'Standing Defense (Standing Guillotine)',
+        'video_url': 'https://www.youtube.com/watch?v=vZJ2JJveVmM&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=33&pp=iAQB'
+    },
+    {
+        'id': '34',
+        'title': 'Standing Defense (Guillotine - Buckle the Knee)',
+        'video_url': 'https://www.youtube.com/watch?v=SHTs6zHWo9o&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=34&pp=iAQB'
+    },
+    {
+        'id': '35',
+        'title': 'Standing Defense (Guillotine - Hands on Knee)',
+        'video_url': 'https://www.youtube.com/watch?v=RjOMPV4Xhq8&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=35&pp=iAQB'
+    },
+    {
+        'id': '36',
+        'title': 'Standing Defense (Lapel Grab - Straight Arm)',
+        'video_url': 'https://www.youtube.com/watch?v=amEgqXSMZEo&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=36&pp=iAQB'
+    },
+    {
+        'id': '37',
+        'title': 'Standing Defense (Lapel Grab - Bent Wrist)',
+        'video_url': 'https://www.youtube.com/watch?v=TdKKDMX9_Iw&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=37&pp=iAQB'
+    },
+    {
+        'id': '38',
+        'title': 'Standing Defense (Lapel Grab - Bent Arm)',
+        'video_url': 'https://www.youtube.com/watch?v=UHupnKt2WEc&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=38&pp=iAQB'
+    },
+    {
+        'id': '39',
+        'title': 'Standing Defense (Two Handed Lapel Grab)',
+        'video_url': 'https://www.youtube.com/watch?v=rWFWjc0WZLE&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=39&pp=iAQB'
+    },
+    {
+        'id': '40',
+        'title': 'Standing Defense (One Handed Wrist Grab)',
+        'video_url': 'https://www.youtube.com/watch?v=ihOT3YzvSm4&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=40&pp=iAQB'
+    },
+    {
+        'id': '41',
+        'title': 'Standing Defense (Two Handed Wrist Grab)',
+        'video_url': 'https://www.youtube.com/watch?v=ZBUl4HtW-oc&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=41&pp=iAQB'
+    },
+    {
+        'id': '42',
+        'title': 'Maintaining Mount (Swimming Arms)',
+        'video_url': 'https://www.youtube.com/watch?v=6EqNfILs8Yc&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=42&pp=iAQB'
+    },
+    {
+        'id': '43',
+        'title': 'Maintaining Mount (Pulling Arms off Knees)',
+        'video_url': 'https://www.youtube.com/watch?v=dL1IkbkIXbU&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=43&pp=iAQB'
+    },
+    {
+        'id': '44',
+        'title': 'Maintaining Mount (Push the Head)',
+        'video_url': 'https://www.youtube.com/watch?v=prU5iFXptzw&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=44&pp=iAQB'
+    },
+    {
+        'id': '45',
+        'title': 'Mount Attack (Basic Armlock)',
+        'video_url': 'https://www.youtube.com/watch?v=t8-yhTW7JGo&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=45&pp=iAQB'
+    },
+    {
+        'id': '46',
+        'title': 'Mount Attack (Americana)',
+        'video_url': 'https://www.youtube.com/watch?v=pX2fOTF08eY&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=46&pp=iAQB'
+    },
+    {
+        'id': '47',
+        'title': 'Mount Attack (Cross Choke)',
+        'video_url': 'https://www.youtube.com/watch?v=Fonr2OK8yK0&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=47&pp=iAQB'
+    },
+    {
+        'id': '48',
+        'title': 'Mount Bottom - (Punch Defense - Bucking)',
+        'video_url': 'https://www.youtube.com/watch?v=jVBACapL2lo&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=48&pp=iAQB'
+    },
+    {
+        'id': '49',
+        'title': 'Escaping Mount (UPA With Choke Defense)',
+        'video_url': 'https://www.youtube.com/watch?v=QMwGYHA53KE&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=49&pp=iAQB'
+    },
+    {
+        'id': '50',
+        'title': 'Escaping Mount (Elbow Escape)',
+        'video_url': 'https://www.youtube.com/watch?v=FWoovJy1a80&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=50&pp=iAQB'
+    },
+    {
+        'id': '51',
+        'title': 'Guard Attacks (Cross Choke)',
+        'video_url': 'https://www.youtube.com/watch?v=MAnkpdiURmk&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=51&pp=iAQB'
+    },
+    {
+        'id': '52',
+        'title': 'Guard Attacks (Armlock)',
+        'video_url': 'https://www.youtube.com/watch?v=rLIrwiLgjwA&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=52&pp=iAQB'
+    },
+    {
+        'id': '53',
+        'title': 'Guard Attacks (Triangle)',
+        'video_url': 'https://www.youtube.com/watch?v=UcOO4yuC7k8&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=53&pp=iAQB'
+    },
+    {
+        'id': '54',
+        'title': 'Guard Attacks (Kimura)',
+        'video_url': 'https://www.youtube.com/watch?v=34gD0NysPzk&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=54&pp=iAQB'
+    },
+    {
+        'id': '55',
+        'title': 'Guard Attacks (Guillotine)',
+        'video_url': 'https://www.youtube.com/watch?v=YFhZ-2o8qUg&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=55&pp=iAQB'
+    },
+    {
+        'id': '56',
+        'title': 'Guard Sweeps (Scissor Sweep to Mount)',
+        'video_url': 'https://www.youtube.com/watch?v=LIQ7ixxZQFw&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=56&pp=iAQB'
+    },
+    {
+        'id': '57',
+        'title': 'Guard Sweeps (Sit Up Sweep)',
+        'video_url': 'https://www.youtube.com/watch?v=OOQJk5RFg6o&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=57&pp=iAQB'
+    },
+    {
+        'id': '58',
+        'title': 'Guard Sweeps (Basic Hook Sweep)',
+        'video_url': 'https://www.youtube.com/watch?v=WBO0I7LeE4A&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=58&pp=iAQB'
+    },
+    {
+        'id': '59',
+        'title': 'Guard Defense (Posture)',
+        'video_url': 'https://www.youtube.com/watch?v=6o3GwavgOkQ&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=59&pp=iAQB'
+    },
+    {
+        'id': '60',
+        'title': 'Guard Defense (Guillotine)',
+        'video_url': 'https://www.youtube.com/watch?v=XH6c84uwuD4&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=60&pp=iAQB'
+    },
+    {
+        'id': '61',
+        'title': 'Guard Defense (Cross Choke - Squeeze the Bread)',
+        'video_url': 'https://www.youtube.com/watch?v=s8KwD1cwwgg&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=61&pp=iAQB'
+    },
+    {
+        'id': '62',
+        'title': 'Guard Defense (Armbar Protection)',
+        'video_url': 'https://www.youtube.com/watch?v=rr-fBndTHuI&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=62&pp=iAQB'
+    },
+    {
+        'id': '63',
+        'title': 'Guard Defense (Cross Choke - Comb the Hair)',
+        'video_url': 'https://www.youtube.com/watch?v=rr-fBndTHuI&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=62&pp=iAQB'
+    },
+    {
+        'id': '64',
+        'title': 'Guard Defense (Choke Protection)',
+        'video_url': 'https://www.youtube.com/watch?v=rr-fBndTHuI&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=62&pp=iAQB'
+    },
+    {
+        'id': '65',
+        'title': 'Guard Pass (Squeeze the Bread)',
+        'video_url': 'https://www.youtube.com/watch?v=rr-fBndTHuI&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=62&pp=iAQB'
+    },
+    {
+        'id': '66',
+        'title': 'Guard Pass (Over Under)',
+        'video_url': 'https://www.youtube.com/watch?v=StJUeDul6NM&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=66&pp=iAQB'
+    },
+    {
+        'id': '67',
+        'title': 'Guard Pass (Standing Up)',
+        'video_url': 'https://www.youtube.com/watch?v=FRwQhFl0_BY&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=67&pp=iAQB'
+    },
+    {
+        'id': '68',
+        'title': 'Cross Body Top (Establish Cross Body)',
+        'video_url': 'https://www.youtube.com/watch?v=Wto_t_NaKGA&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=68&pp=iAQB'
+    },
+    {
+        'id': '69',
+        'title': 'Cross Body Top (Maintaining Cross Body)',
+        'video_url': 'https://www.youtube.com/watch?v=r0Go7i2KjI8&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=69&pp=iAQB'
+    },
+    {
+        'id': '70',
+        'title': 'Cross Body Top (Attack - Chicken Wing from Kimura)',
+        'video_url': 'https://www.youtube.com/watch?v=hreqEpUKIqU&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=70&pp=iAQB'
+    },
+    {
+        'id': '71',
+        'title': 'Cross Body Top (Attack - Americana)',
+        'video_url': 'https://www.youtube.com/watch?v=SeWRtCPh9IM&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=71&pp=iAQB'
+    },
+    {
+        'id': '72',
+        'title': 'Cross Body Top (Attack - Basic Armlock)',
+        'video_url': 'https://www.youtube.com/watch?v=AlsErGapGqQ&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=72&pp=iAQB'
+    },
+    {
+        'id': '73',
+        'title': 'Cross Body Top (Mount - Arm Out)',
+        'video_url': 'https://www.youtube.com/watch?v=wvrds8dfgNY&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=73&pp=iAQB'
+    },
+    {
+        'id': '74',
+        'title': 'Cross Body Top (Mount - Arm In)',
+        'video_url': 'https://www.youtube.com/watch?v=PczMvzwxGcI&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=74&pp=iAQB'
+    },
+    {
+        'id': '75',
+        'title': 'Cross Body Bottom (Regaining the Guard)',
+        'video_url': 'https://www.youtube.com/watch?v=_1RM3N7kkm4&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=75&pp=iAQB'
+    },
+    {
+        'id': '76',
+        'title': 'Cross Body Bottom (Turn on Knees)',
+        'video_url': 'https://www.youtube.com/watch?v=hH9l7W1dLLI&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=76&pp=iAQB'
+    },
+    {
+        'id': '77',
+        'title': 'Cross Body Bottom (Headlock Defense - Opponents Head Low)',
+        'video_url': 'https://www.youtube.com/watch?v=QrPPh-tli_8&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=77&pp=iAQB'
+    },
+    {
+        'id': '78',
+        'title': 'Cross Body Bottom (Head & Arm Defense - Hips Under Opponent)',
+        'video_url': 'https://www.youtube.com/watch?v=ysFS_1QZEFM&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=78&pp=iAQB'
+    },
+    {
+        'id': '79',
+        'title': 'Cross Body Bottom (Headlock Defense - Make a Frame)',
+        'video_url': 'https://www.youtube.com/watch?v=nOaoI5fi2sY&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=79&pp=iAQB'
+    },
+    {
+        'id': '80',
+        'title': 'Cross Body Bottom (Headlock Defense - Go to Back)',
+        'video_url': 'https://www.youtube.com/watch?v=pydDRWpeqQQ&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=80&pp=iAQB'
+    },
+    {
+        'id': '81',
+        'title': 'Cross Body Bottom (Headlock Defense - Go to Knees)',
+        'video_url': 'https://www.youtube.com/watch?v=1h98iWW6Dt4&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=81&pp=iAQB'
+    },
+    {
+        'id': '82',
+        'title': 'Cross Body Bottom (Neck Crank Defense - Pass the Leg)',
+        'video_url': 'https://www.youtube.com/watch?v=Fo-RInKpyPQ&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=82&pp=iAQB'
+    },
+    {
+        'id': '83',
+        'title': 'Cross Body Bottom (Mount Defense - Move Away)',
+        'video_url': 'https://www.youtube.com/watch?v=8XE23K_12ec&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=83&pp=iAQB'
+    },
+    {
+        'id': '84',
+        'title': 'Back Top (Attack - Rear Naked Choke "Mata Leao")',
+        'video_url': 'https://www.youtube.com/watch?v=RTTUOPdMMyo&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=84&pp=iAQB'
+    },
+    {
+        'id': '85',
+        'title': 'Back Top (Attack - Choke with collar)',
+        'video_url': 'https://www.youtube.com/watch?v=0oDkr-0O09k&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=85&pp=iAQB'
+    },
+    {
+        'id': '86',
+        'title': 'Back Top (Maintain Control with Hooks)',
+        'video_url': 'https://www.youtube.com/watch?v=MySJqqK6saA&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=86&pp=iAQB'
+    },
+    {
+        'id': '87',
+        'title': 'Back Bottom (Escape - Back to Floor)',
+        'video_url': 'https://www.youtube.com/watch?v=ZCn_rMGNq88&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=87&pp=iAQB'
+    },
+    {
+        'id': '88',
+        'title': 'Back Bottom (Escape - Finger Point)',
+        'video_url': 'https://www.youtube.com/watch?v=t5dtKpsksYM&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=88&pp=iAQB'
+    },
+    {
+        'id': '89',
+        'title': 'Standing Headlock Esapes (3 Variations)',
+        'video_url': 'https://www.youtube.com/watch?v=KqjlQ1URdZw&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=89&pp=iAQB'
+    },
+    {
+        'id': '90',
+        'title': 'Backward sacrifice movement from standing headlock.',
+        'video_url': 'https://www.youtube.com/watch?v=OK1dan0bkF8&list=PLPR9Y8aStUajqgDzU3Dc_VfCdO6VK9Wf1&index=90&pp=iAQB'
+    },
 ]
